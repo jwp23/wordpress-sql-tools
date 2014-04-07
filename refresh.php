16175ddaafe7f7f739ab46c7230aceeb6b283e9f
Joseph Presley <jpresley@beachbody.com>
2014-04-07 14:42:45 -0700

    moved config for file for baseline database dump to config.php and changed dump file name to 24 hour format

--- refresh.php
+++ refresh.php
@@ -2,8 +2,6 @@
 include_once('functions.php');
 include_once('config.php');
 
-define (PRODUCTION_DB_DUMP_FILE, "wordpressunified-2014-03-31-17-14-09.sql");
-
 # refresh local copy of wordpressunified db
 refresh_db(LOCAL_SERVER, LOCAL_USER, LOCAL_PASSWORD, LOCAL_WORDPRESS_DB, PRODUCTION_DB_DUMP_FILE);
 
