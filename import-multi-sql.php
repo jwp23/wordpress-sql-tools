188ddf9024f5dd45ce8643ab28af8dc055613950
Joseph Presley <jpresley@beachbody.com>
2014-04-17 11:46:03 -0700

    added ability to comment lines in the import file

--- import-multi-sql.php
+++ import-multi-sql.php
@@ -5,6 +5,7 @@ include_once('config.php');
 # import list of files
 $file_contents_raw=file(IMPORT_FILES);
 $file_contents_neat=array();
+$file_removed_commented_lines=array();
 $file_names_full=array();
 
 # trim end of line character
@@ -13,8 +14,17 @@ foreach ($file_contents_raw as $key => $file_raw)
 	$file_contents_neat[$key] = rtrim($file_raw);
 }
 
+# remove commented lines
+$i = 0; //set index
+foreach ($file_contents_neat as $file_lines)
+{
+	if (substr($file_lines, 0, 1) != '#') {
+		$file_removed_commented_lines[$i] = $file_lines;
+		$i = $i + 1; //increase index
+	} 
+}
 # add path prefix to file names
-foreach ($file_contents_neat as $key => $file_neat)
+foreach ($file_removed_commented_lines as $key => $file_neat)
 {
 	$file_names_full[$key]=LOCAL_PREFIX . $file_neat;
 }
@@ -23,10 +33,12 @@ foreach ($file_contents_neat as $key => $file_neat)
 
 foreach ($file_names_full as $sql)
 {
+	 
 	$return=import_sql(LOCAL_SERVER, LOCAL_USER, LOCAL_PASSWORD, LOCAL_WORDPRESS_DB, $sql);
-	
+
 	if ($return == 1) {
 		exit ('Import file caused sql error. Exit script.');
 	}
+	
 }
 ?>
