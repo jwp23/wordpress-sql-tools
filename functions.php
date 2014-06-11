0fccf0cd2623dc63d16d5f45902e4c79cb3daaee
Joseph Presley <jpresley@beachbody.com>
2014-06-11 12:47:33 -0700

    Added check_db() to check existence of database

--- functions.php
+++ functions.php
@@ -18,6 +18,18 @@ function exec_command($command)
 	return $return_value;
 }
 
+function check_db($check_server, $check_user, $check_password, $check_db)
+{
+	$check = MYSQL_PATH . "mysql -h $check_server -u $check_user -p'$check_password' '$check_db' 2>&1" ;
+	$return_check = exec_command($check);
+	if ($return_check===0){
+		echo "Database $check_db on $check_server exists<br />";
+		return 1;
+	} elseif ($return_check===1) {
+		echo "Database $check_db on $check_server does not exist<br />";
+		return 0;
+	}
+}
 function dump_db($dump_server, $dump_user, $dump_password, $dump_db, $dump_sql)
 {
 	$dump= MYSQL_PATH . "mysqldump -h $dump_server -u $dump_user -p'$dump_password' $dump_db 2>&1 > $dump_sql";
@@ -27,7 +39,12 @@ function dump_db($dump_server, $dump_user, $dump_password, $dump_db, $dump_sql)
 function drop_db($drop_server, $drop_user, $drop_password, $drop_db)
 {
 	$drop= MYSQL_PATH . "mysqladmin -h $drop_server -u $drop_user -p$drop_password -f drop $drop_db 2>&1";
-	$return_drop = exec_command($drop);
+	if (check_db($drop_server, $drop_user, $drop_password, $drop_db)===1) {
+		$return_drop = exec_command($drop);
+	} else {
+		$return_drop = 0;
+	}
+	
 	return $return_drop;
 }
 
@@ -51,7 +68,7 @@ function refresh_db($refresh_server, $refresh_user, $refresh_password, $refresh_
 	#drop database
 	$return_drop = drop_db($refresh_server, $refresh_user, $refresh_password, $refresh_db);
 	if ($return_drop == 1) {
-		exit ('Dump database ' . $refresh_db . ' failed. Exit script.');
+		exit ('Drop database ' . $refresh_db . ' failed. Exit script.');
 	}
 	
 	#create empty database
