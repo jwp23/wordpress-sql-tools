a8da70feb81a1ef921cfba2e3e07a0b2f905a8da
Joseph Presley <jpresley@beachbody.com>
2015-09-27 18:19:51 -0700

    remove confidential information from config file

--- config.php
+++ config.php
@@ -1,22 +1,22 @@
 <?php
 
 # production database server information
-define ("PRODUCTION_SERVER", "pwop-mys000.dc1.beachbody.com");
-define ("PRODUCTION_USER", "wpuser");
-define ("PRODUCTION_PASSWORD", "b3achb0dy!");
-define ("PRODUCTION_DB", "wordpressunified");
-define ("PRODUCTION_DB_DUMP_FILE", "wordpressunified-2014-04-11-09-11-59.sql");
+define ("PRODUCTION_SERVER", "");
+define ("PRODUCTION_USER", "");
+define ("PRODUCTION_PASSWORD", "");
+define ("PRODUCTION_DB", "");
+define ("PRODUCTION_DB_DUMP_FILE", "");
 
 # local database environment information
-define ("LOCAL_SERVER", "localhost");
-define ("LOCAL_USER", "root");
-define ("LOCAL_PASSWORD", "un1c0rn23");
-define ("LOCAL_PRODUCTION_COPY_DB", "WPU-LOCAL"); //name of database of local copy of production database
-define ("LOCAL_WORDPRESS_DB", "wordpressunified"); //name of local database used for wordpress
+define ("LOCAL_SERVER", "");
+define ("LOCAL_USER", "");
+define ("LOCAL_PASSWORD", "");
+define ("LOCAL_PRODUCTION_COPY_DB", ""); //name of database of local copy of production database
+define ("LOCAL_WORDPRESS_DB", ""); //name of local database used for wordpress
 
 
-define ("UPDATE_LOCAL_SITES", "update-to-local.sql"); // sql file to modify production site urls to local urls
+define ("UPDATE_LOCAL_SITES", ""); // sql file to modify production site urls to local urls
 
-define ("LOCAL_PREFIX", "/Users/jpresley/Sites/wordpressunified/sql/");
+define ("LOCAL_PREFIX", ""); // path prefix aka directory path for sql files to apply to database after UPDATE_LOCAL_SITES file is applied
 define ("IMPORT_FILES", "import-files.txt");
 define ("MYSQL_PATH", "/usr/local/mysql/bin/");
